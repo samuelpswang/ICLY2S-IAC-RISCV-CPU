b571e4aefa059bd89c4bfdc82e4d7286a2c3be99
Chenglin-Sun <116260478+Chenglin-Sun@users.noreply.github.com>
2022-12-04 15:13:24 +0000

    feat: add data_memory.sv

---
+++ source/mem/data_memory.sv
@@ -0,0 +1,20 @@
+module data_memory (
+  input logic clk,
+  input logic [31:0] A,
+  input logic WE,
+  input logic [31:0] WD,
+  output logic [31:0] RD
+);
+
+logic [11:0] AD;
+logic [31:0] mem_data [2**12 - 1:0]; 
+
+always_comb begin
+    AD = A[11:0];
+    RD = mem_data[AD];
+end
+  
+always_ff @ (posedge clk)
+  if (WE) mem_data[AD] <= WD; 
+
+endmodule
