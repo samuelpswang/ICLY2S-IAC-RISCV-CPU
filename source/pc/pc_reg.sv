207de66e06f95aa7c7cabc01c3536a921b0734e0
QDZHOU <97557199+QDZHOU@users.noreply.github.com>
2022-12-05 21:01:16 +0000

    fix: fix fault in pc_reg

--- source/pc/pc_reg.sv
+++ source/pc/pc_reg.sv
@@ -6,16 +6,16 @@ module pc_reg #(
   input logic clk,
   input logic rst,
   input logic PCsrc,
-  input logic [IMMO_WIDTH-1:0] ImmoOp,
+  input logic [IMMO_WIDTH-1:0] ImmOp,
   output logic [ADDR_WIDTH-1:0] pc
 );
 
 logic [ADDR_WIDTH-1:0] next_pc;
 
-assign next_pc = (PCsrc ? {pc + ImmOp} : {pc + 32'd4});
+assign next_pc = (PCsrc ? pc + ImmOp : pc + 32'd4);
 
 always_ff @(posedge clk) begin
-  if (rst|pc == {8{1'hF}}) pc <= {3'hBFC,ADDR_WIDTH-12{1'b0}}; 
+  if (rst||pc == {8{4'hF}}) pc <= {32'hBFC00000}; 
   pc <= next_pc;
 end
 
