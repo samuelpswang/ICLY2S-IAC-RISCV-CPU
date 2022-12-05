cfe539e94bd5f295b2f287ececcc09d6a8a76ac6
QDZHOU <97557199+QDZHOU@users.noreply.github.com>
2022-12-05 23:47:06 +0000

    feat: alu_top_tb

--- testbench/alu/alu_tb.cpp
+++ testbench/alu/alu_tb.cpp
@@ -2,48 +2,34 @@
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 
-int main(int argc, char **argv, char **env){
-    int it;
+int main(int agrc, char **argv, char **env){
+    int i;
     int clk;
-    Verilated::commandArgs(argc,argv);
-//  initialise top verilog instance
+
+    Verilated::commandArgs(agrc, argv);
     Valu_top* top = new Valu_top;
-// initialise trace dump
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
-    top->trace(tfp,99);
-    tfp->open("Valu_top.vcd");
+    top->trace (tfp, 99);
+    tfp->open ("alu_top.vcd");
 
-    // initialise simulation outputs
-    top->clk =1;
+    top->clk = 1;
     top->ALUsrc = 1;
-    top->ALUctrl = 0;
-    top->ImmOp = 0xFF;
-    top->Instr = 0x0FF00013;
+    top->ALUctrl = 0b1111;
+    top->Instr = 0x00090900;
     top->RegWrite = 1;
+    top->ImmOp = 0x0000001;
 
-    // run simulation for many clock cycles
-    int tick = 0;
-    for(int i=0;i< 300; i++){
-        // Add to readme, remember to compelete part 2 of challenge
-        // dump variables into VCD file and toggle clock
-        for(clk=0;clk<2;clk++){
-            // in ps
-            tfp->dump (2*i+clk);
-            // falling edge
+    for(i = 0; i<11; i++){
+        for(clk = 0; clk<2; clk++){
+            tfp->dump(2*i+clk);
             top->clk = !top->clk;
-            top->eval ();
+            top->eval();
         }
-        top->clk =1;
-        top->ALUsrc = 0;
-        top->ALUctrl = 0;
-        top->ImmOp = 0;
-        top->Instr = 0;
-        top-> RegWrite = 1;
+        top->ALUctrl += 0b0001;
+        top->RegWrite = (i == 0);
         if(Verilated::gotFinish()) exit(0);
-        
     }
     tfp->close();
     exit(0);
-
 }
