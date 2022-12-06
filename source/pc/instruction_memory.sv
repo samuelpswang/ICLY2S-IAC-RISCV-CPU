0fdd2ed6986ed0a55dc324e138563226a5b2c8fa
Samuel Wang <samuelpswang@gmail.com>
2022-12-06 18:11:09 +0000

    feat: add instruction memory component

---
+++ source/pc/instruction_memory.sv
@@ -0,0 +1,18 @@
+module instr_mem #(
+  parameter INSTR_WIDTH = 32,
+  parameter ADDR_WIDTH = 32
+)(
+  input logic [ADDR_WIDTH-1:0] A,
+  output logic [INSTR_WIDTH-1:0] RD
+);
+
+logic [INSTR_WIDTH-1:0] instr_data [(2**8-1):0];
+
+initial begin
+  $display("[DUT] Load instr_mem from f1_branch.mem file.");
+  $readmemh("../../program/f1/f1_branch.mem", instr_data);
+end
+
+assign RD = instr_data[A[9:2]];
+
+endmodule
