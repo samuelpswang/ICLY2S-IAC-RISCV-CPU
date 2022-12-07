6837355ea53351d874e91fa520c3b4144141b5bd
Samuel Wang <samuelpswang@gmail.com>
2022-12-07 12:44:34 +0000

    fix: update f1 program

--- program/f1/f1_branch.asm
+++ program/f1/f1_branch.asm
@@ -1,9 +1,11 @@
 main:   
     addi    s0, zero, 0x1
+    addi    a0, zero, 0x7F
+    addi    a1, zero, 0x0
     bne     zero, s0, lfsr    
 start:  
     addi    a0, zero, 0x1       # a0: lights state
-    addi    s1, zero, 0xF       # s1: lights final state
+    addi    s1, zero, 0xFF      # s1: lights final state
     addi    s2, zero, 0x1
     slli    s2, s2, 0xC         # s2: random data location
     addi    s3, zero, 0x8       # s3: random data final state
@@ -30,21 +32,21 @@ lfsr:
     addi    t3, zero, 0x0
     addi    t4, zero, 0x0
 nloop:                          # next loop
-    addi    a0, t1, 0x0         # shift and add to get numbers
+    addi    a1, t1, 0x0         # shift and add to get numbers
     slli    t5, t2, 0x1
-    add     a0, a0, t5
+    add     a1, a1, t5
     slli    t5, t3, 0x2
-    add     a0, a0, t5
+    add     a1, a1, t5
     slli    t5, t4, 0x3
-    add     a0, a0, t5
+    add     a1, a1, t5
     xor     t6, t3, t4          # xor set up next state
     add     t4, t3, zero
     add     t3, t2, zero
     add     t2, t1, zero
     add     t1, t6, zero
-    sw      a0, 0(s1)           # store and next
+    sw      a1, 0(s1)           # store and next
     addi    s1, s1, 0x4
-    bne     a0, s2, nloop
+    bne     a1, s2, nloop
     bne     zero, s0, start
 
 end:
