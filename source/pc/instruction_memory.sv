3333c79118bdde19d75be036a85461cf1ecce180
Samuel Wang <samuelpswang@gmail.com>
2022-12-07 12:45:27 +0000

    fix: instruction memory load address

--- source/pc/instruction_memory.sv
+++ source/pc/instruction_memory.sv
@@ -9,8 +9,8 @@ module instruction_memory #(
 logic [INSTR_WIDTH-1:0] instr_data [(2**8-1):0];
 
 initial begin
-  $display("[DUT] Load instr_mem from f1_branch.mem file.");
-  $readmemh("../../program/f1/f1_branch.mem", instr_data);
+  $display("[DUT] Load instr_data from f1_branch.mem file.");
+  $readmemh("../program/f1/f1_branch.mem", instr_data);
 end
 
 assign RD = instr_data[A[9:2]];
