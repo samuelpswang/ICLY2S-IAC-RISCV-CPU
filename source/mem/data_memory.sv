5b5afbe993bae046632a3b1dc0e9e39eb79af568
BharathaanSukumaran <bharathaansukumaran@gmail.com>
2022-12-08 14:58:22 +0000

    fix: fixed address width and naming issue

--- source/mem/data_memory.sv
+++ source/mem/data_memory.sv
@@ -1,4 +1,4 @@
-module dataory #(
+module data_memory #(
   parameter WORD_WIDTH = 8,
   parameter ADDR_WIDTH = 17,
   parameter DATA_WIDTH = 32
@@ -13,20 +13,20 @@ module dataory #(
 logic [WORD_WIDTH-1:0] data [2**ADDR_WIDTH-1:0];
 
 always_comb begin
-  if (A[2:0] == 2'b00) RD = {data[A[ADDR_WIDTH-1:0]+3], data[A[ADDR_WIDTH-1:0]+2], data[A[ADDR_WIDTH-1:0]+1], data[A[ADDR_WIDTH-1:0]]};
-  else if (A[2:0] == 2'b10) RD = {data[A[ADDR_WIDTH-1:0]+1], data[A[ADDR_WIDTH-1:0]]};
+  if (A[1:0] == 2'b00) RD = {data[A[ADDR_WIDTH-1:0]+3], data[A[ADDR_WIDTH-1:0]+2], data[A[ADDR_WIDTH-1:0]+1], data[A[ADDR_WIDTH-1:0]]};
+  else if (A[1:0] == 2'b10) RD = {{16{1'b0}},data[A[ADDR_WIDTH-1:0]+1], data[A[ADDR_WIDTH-1:0]]};
   else RD = {24'b0, data[A[ADDR_WIDTH-1:0]]};
 end
 
 always_ff @ (posedge clk) begin
   if (WE) begin
-      if (A[2:0] == 2'b00) begin
+      if (A[1:0] == 2'b00) begin
         data[A[ADDR_WIDTH-1:0]+3] <= WD[WORD_WIDTH*4-1:WORD_WIDTH*3];
         data[A[ADDR_WIDTH-1:0]+2] <= WD[WORD_WIDTH*3-1:WORD_WIDTH*2];
         data[A[ADDR_WIDTH-1:0]+1] <= WD[WORD_WIDTH*2-1:WORD_WIDTH];
         data[A[ADDR_WIDTH-1:0]] <= WD[WORD_WIDTH-1:0];
       end
-      else if (A[2:0] == 2'b10) begin
+      else if (A[1:0] == 2'b10) begin
         data[A[ADDR_WIDTH-1:0]+1] <= WD[WORD_WIDTH*2-1:WORD_WIDTH];
         data[A[ADDR_WIDTH-1:0]] <= WD[WORD_WIDTH-1:0];
       end
