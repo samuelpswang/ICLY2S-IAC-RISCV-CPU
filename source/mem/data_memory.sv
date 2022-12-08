fe512e79f22ba4968b71352c5cadccaf8e3fa23a
Samuel Wang <samuelpswang@gmail.com>
2022-12-08 14:18:22 +0000

    fix: add byte addressing for memories

--- source/mem/data_memory.sv
+++ source/mem/data_memory.sv
@@ -1,21 +1,39 @@
-module data_memory (
+module dataory #(
+  parameter WORD_WIDTH = 8,
+  parameter ADDR_WIDTH = 17,
+  parameter DATA_WIDTH = 32
+)(
   input logic clk,
-  input logic [31:0] A,
+  input logic [DATA_WIDTH-1:0] A,
   input logic WE,
-  input logic [31:0] WD,
-  output logic [31:0] RD
+  input logic [DATA_WIDTH-1:0] WD,
+  output logic [DATA_WIDTH-1:0] RD
 );
 
-logic [11:0] AD;
-logic [31:0] mem_data [2**12 - 1:0]; 
+logic [WORD_WIDTH-1:0] data [2**ADDR_WIDTH-1:0];
 
-always_comb
-    AD = A[11:0];
+always_comb begin
+  if (A[2:0] == 2'b00) RD = {data[A[ADDR_WIDTH-1:0]+3], data[A[ADDR_WIDTH-1:0]+2], data[A[ADDR_WIDTH-1:0]+1], data[A[ADDR_WIDTH-1:0]]};
+  else if (A[2:0] == 2'b10) RD = {data[A[ADDR_WIDTH-1:0]+1], data[A[ADDR_WIDTH-1:0]]};
+  else RD = {24'b0, data[A[ADDR_WIDTH-1:0]]};
+end
 
-always_comb
-    RD = mem_data[AD];
-  
-always_ff @ (posedge clk)
-  if (WE) mem_data[AD] <= WD; 
+always_ff @ (posedge clk) begin
+  if (WE) begin
+      if (A[2:0] == 2'b00) begin
+        data[A[ADDR_WIDTH-1:0]+3] <= WD[WORD_WIDTH*4-1:WORD_WIDTH*3];
+        data[A[ADDR_WIDTH-1:0]+2] <= WD[WORD_WIDTH*3-1:WORD_WIDTH*2];
+        data[A[ADDR_WIDTH-1:0]+1] <= WD[WORD_WIDTH*2-1:WORD_WIDTH];
+        data[A[ADDR_WIDTH-1:0]] <= WD[WORD_WIDTH-1:0];
+      end
+      else if (A[2:0] == 2'b10) begin
+        data[A[ADDR_WIDTH-1:0]+1] <= WD[WORD_WIDTH*2-1:WORD_WIDTH];
+        data[A[ADDR_WIDTH-1:0]] <= WD[WORD_WIDTH-1:0];
+      end
+      else begin
+        data[A[ADDR_WIDTH-1:0]] <= WD[WORD_WIDTH-1:0];
+      end
+  end
+end
 
 endmodule
--- source/pc/instruction_memory.sv
+++ source/pc/instruction_memory.sv
@@ -1,18 +1,22 @@
 module instruction_memory #(
-  parameter INSTR_WIDTH = 32,
-  parameter ADDR_WIDTH = 32
+  parameter WORD_WIDTH = 8,
+  parameter ADDR_WIDTH = 12,
+  parameter INSTR_WIDTH = 32
 )(
-  input logic [ADDR_WIDTH-1:0] A,
+  input logic [INSTR_WIDTH-1:0] A,
   output logic [INSTR_WIDTH-1:0] RD
 );
 
-logic [INSTR_WIDTH-1:0] instr_data [(2**8-1):0];
+logic [WORD_WIDTH-1:0] instr_data [2**ADDR_WIDTH-1:0];
 
 initial begin
-  $display("[DUT] Load instr_data from f1_branch.hex file.");
+  $display("[DUT] Loading instr_data from f1_branch.hex file.");
   $readmemh("../program/f1/f1_branch.hex", instr_data);
 end
 
-assign RD = instr_data[A[9:2]];
+always_comb begin
+  if (A[2:0] == 2'b00) RD = {instr_data[A[ADDR_WIDTH-1:0]+3], instr_data[A[ADDR_WIDTH-1:0]+2], instr_data[A[ADDR_WIDTH-1:0]+1], instr_data[A[ADDR_WIDTH-1:0]]};
+  else RD = {24'b0, instr_data[A[ADDR_WIDTH-1:0]]};
+end
 
 endmodule
