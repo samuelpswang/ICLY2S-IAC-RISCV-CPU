036cda3154c0f5a2273ef57c637c852c453e7856
BharathaanSukumaran <bharathaansukumaran@gmail.com>
2022-12-08 00:20:44 +0000

    feat: added extension for J-type instruction

--- source/ctrl/extend.sv
+++ source/ctrl/extend.sv
@@ -7,12 +7,12 @@ module extend(
 // 00: Sign Extend I-type
 // 01: Sign Extend B-type
 // 10: Sign Extend S-type
-// 11: Output 0
+// 11: Sign Extend J-type
 always_comb begin
   if (ImmSrc == 2'b00) ImmExt = {{20{Instr[31]}}, Instr[31:20]};
   else if (ImmSrc == 2'b01) ImmExt = {{19{Instr[31]}}, Instr[31], Instr[7], Instr[30:25], Instr[11:8], 1'b0};
   else if (ImmSrc == 2'b10) ImmExt = {{20{Instr[31]}}, Instr[31:25], Instr[11:7]};
-  else ImmExt = 32'd0;
+  else if (ImmSrc ==  2'b11) ImmExt = {{12{Instr[31]}},Instr[19:12],Instr[20],Instr[30:21], 1'b0};
 end
   
 endmodule
