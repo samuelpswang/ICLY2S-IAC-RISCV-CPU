ddd35a5dde0abd2ced9a191f7a5d36d99f5da8ed
BharathaanSukumaran <bharathaansukumaran@gmail.com>
2022-12-08 00:24:49 +0000

    fix: changed naming convention for jumps

--- source/pc/pc_reg.sv
+++ source/pc/pc_reg.sv
@@ -5,13 +5,13 @@ module pc_reg #(
   input logic clk,
   input logic rst,
   input logic PCsrc,
-  input logic [IMMO_WIDTH-1:0] ImmOp,
+  input logic [IMMO_WIDTH-1:0] label,
   output logic [ADDR_WIDTH-1:0] pc
 );
 
 logic [ADDR_WIDTH-1:0] next_pc;
 
-assign next_pc = (PCsrc ? pc + ImmOp : pc + 32'd4);
+assign next_pc = (PCsrc ? pc + label : pc + 32'd4);
 
 always_ff @(posedge clk) begin
   if (rst||pc == {8{4'hF}}) pc <= {32'hBFC00000}; 
