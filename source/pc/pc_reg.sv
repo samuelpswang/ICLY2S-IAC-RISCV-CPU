13407885caaca905c03407f629ed3b6d5e3e3bd3
BharathaanSukumaran <bharathaansukumaran@gmail.com>
2022-12-08 13:53:14 +0000

    fix: fixed bug for jalr instruction

--- source/pc/pc_reg.sv
+++ source/pc/pc_reg.sv
@@ -11,7 +11,7 @@ module pc_reg #(
 
 logic [ADDR_WIDTH-1:0] next_pc;
 
-assign next_pc = (PCsrc ? pc + label : pc + 32'd4);
+assign next_pc = (PCsrc ? label : pc + 32'd4);
 
 always_ff @(posedge clk) begin
   if (rst||pc == {8{4'hF}}) pc <= {32'hBFC00000}; 
