851b09a034a7760db63d6db29a26336ab56d689e
BharathaanSukumaran <bharathaansukumaran@gmail.com>
2022-12-08 00:18:47 +0000

    feat: added operation for jalr

--- source/alu/alu.sv
+++ source/alu/alu.sv
@@ -5,28 +5,38 @@ module alu (
   input logic [31:0] ALUop1,
   input logic [31:0] regOp2,
   output logic EQ,
-  output logic [31:0] SUM // note: 31:1 -> 31:0
+  output logic [31:0] SUM 
 );
 
-// note: use correct indentation
-logic [31:0] ALUop2;
 
+logic [31:0] ALUop2;
+logic [31:0] JALR_INTERMEDIATE;
 assign ALUop2 = ALUsrc? ImmOp:regOp2;
-  
-    
-// note: unify number format
+assign JALR_INTERMEDIATE = ALUop1 + ALUop2;
+
 
 always_comb begin
     case (ALUctrl)
-    4'b0000: SUM = ALUop1+ALUop2;
+    // add 
+    4'b0000: SUM = ALUop1 + ALUop2;
+    // sub
     4'b0001: SUM = ALUop1 - ALUop2;
+    // and 
     4'b0010: SUM = ALUop1 & ALUop2;
+    // or 
     4'b0011: SUM = ALUop1 | ALUop2;
+    // xor 
     4'b0100: SUM = ALUop1^ALUop2;
-    4'b0101: SUM = {{31{1'b0}},(ALUop1<ALUop2)};
+    // slt 
+    4'b0101: SUM = {{31{1'b0}} , (signed'(ALUop1)<signed'(ALUop2))};
+    // sll 
     4'b0110: SUM = ALUop1 << ALUop2[4:0];
+    // srl
     4'b0111: SUM = ALUop1 >> ALUop2[4:0];
+    // sra 
     4'b1000: SUM = ALUop1 >>> ALUop2[4:0];
+    // jalr
+    4'b1001: SUM = {JALR_INTERMEDIATE[31:1],1'b0};
     
     default: begin
       SUM = 32'b0;
