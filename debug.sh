84fff7716ea9105e8df0c4ce0f8db1f526cbda34
Samuel Wang <samuelpswang@gmail.com>
2022-12-10 14:43:07 +0000

    feat: add reference program

--- debug.sh
+++ debug.sh
@@ -1,11 +1,12 @@
 # cleanup
 rm -rf build
 rm -f Vrtl.vcd
+make -f program/f1.mk clean
+make -f program/ref.mk clean
 
 # generate .hex file
-cd program
-make hexfile
-cd ..
+make -f program/f1.mk hexfile
+make -f program/ref.mk reference
 
 # verilator
 verilator -F debug.cfg --cc --trace source/rtl.sv --exe testbench/rtl/rtl_tb.cpp
