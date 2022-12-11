f9c52e70a42658fb50e13ca3dba40b2172ec9bd0
Samuel Wang <samuelpswang@gmail.com>
2022-12-11 16:11:06 +0000

    fix: edit parameters in reference program

--- program/ref/pdf.s
+++ program/ref/pdf.s
@@ -1,7 +1,7 @@
 .text
 .equ base_pdf, 0x100
 .equ base_data, 0x10000
-.equ max_count, 200
+.equ max_count, 255
 main:
     JAL     ra, init  # jump to init, ra and save position to ra
     JAL     ra, build
@@ -13,7 +13,7 @@ init:       # function to initialise PDF buffer memory
     LI      a1, 0xff            # loop_count a1 = 255
 _loop1:                         # repeat
     SB      zero, base_pdf(a1)  #     mem[base_pdf+a1) = 0
-    ADDI    a1, zero, -1        #     decrement a1
+    ADDI    a1, a1, -1        #     decrement a1
     BNE     a1, zero, _loop1    # until a1 = 0
     RET
 
