0fd678f1de18c43019fd655d7ebfe7fdf4d03a7b
Samuel Wang <samuelpswang@gmail.com>
2022-12-11 16:33:02 +0000

    fix: fix minor syntax errors in rtl and ref tb

--- testbench/rtl/reference_tb.cpp
+++ testbench/rtl/reference_tb.cpp
@@ -1,32 +1,32 @@
 #include "Vrtl.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-// #include"vbuddy.cpp"
+#include "vbuddy.cpp"
 
 int main(int argc, char **argv, char **env){
-    int it;
+    // init variables
     int clk;
+
+    // init verilog instance
     Verilated::commandArgs(argc,argv);
-//  initialise top verilog instance
     Vrtl* top = new Vrtl;
-// initialise trace dump
+
+    // init trace
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
     top->trace(tfp,99);
     tfp->open("Vrtl.vcd");
-    // init Vbuddy
-    // if (vbdOpen()!=1) return (-1);
-    // vbdHeader("F1 Lights");
-    // initialise simulation outputs
-    top->clk =1;
-    top->rst=0;
 
+    // init vbuddy
+    if (vbdOpen() != 1) return 1;
+    vbdHeader("PDF Plot");
+
+    // initial variable state
+    top->clk = 1;
+    top->rst = 0;
 
     // run simulation for many clock cycles
-    int tick = 0;
-    for(int i=0;i< 100000; i++){
-        // Add to readme, remember to compelete part 2 of challenge
-        // dump variables into VCD file and toggle clock
+    for (int i = 0; i < 1000000; i++) {
         for(clk=0;clk<2;clk++){
             // in ps
             tfp->dump (2*i+clk);
@@ -34,17 +34,13 @@ int main(int argc, char **argv, char **env){
             top->clk = !top->clk;
             top->eval ();
         }
-        
-        top->clk =1;
-        top->rst=0;
-        if(i > 50000) vbdPlot(int(top->a0),0,255);
-        vbdCycle(i+1);
-        
-        if(Verilated::gotFinish()) exit(0);
-        
+
+        if (i > 800000) vbdPlot(int(top->a0), 0, 255);
+        if (Verilated::gotFinish()) exit(0);
     }
-    // vbdClose();
+
+    // housekeeping
+    vbdClose();
     tfp->close();
     exit(0);
-
 }
