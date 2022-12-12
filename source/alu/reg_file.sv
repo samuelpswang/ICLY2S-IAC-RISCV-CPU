f00b58eac46fe9f87cd3a32aeaa5980e736103ae
BharathaanSukumaran <bharathaansukumaran@gmail.com>
2022-12-12 15:26:34 +0000

    fix: fixed bug when writing to register

--- source/alu/reg_file.sv
+++ source/alu/reg_file.sv
@@ -22,7 +22,9 @@ always_comb begin
 end
 
 always_ff @ (posedge clk)
-  if (WE3 && AD3 != 5'd0) reg_data[AD3] <= WD3; 
-  else reg_data[AD3] <= 32'b0;
+  if (WE3) begin
+    if (AD3 == 5'b0) reg_data[AD3] <= 32'b0; 
+    else reg_data[AD3] <= WD3; 
+  end
 
 endmodule
