976987bd6600e33a8d854efa2f9bec333d035a1d
Chenglin-Sun <116260478+Chenglin-Sun@users.noreply.github.com>
2022-12-12 22:46:28 +0000

    feat: set up dirty bit for data_cache.sv
    Also fix write condition, taking hit into consideration.

--- source/mem/data_cache.sv
+++ source/mem/data_cache.sv
@@ -5,6 +5,7 @@ module data_cache #(
     input logic clk,
     input logic WEE, // Write enable from outside
     input logic WEC, // Write enable for the cache
+    input logic hit,
     input logic [24:0] tag,
     input logic [2:0] set_num,
     input logic [1:0] block_offset,
@@ -24,30 +25,36 @@ logic [DATA_WIDTH-1:0] data_reg_2 [7:0];
 logic [DATA_WIDTH-1:0] data_reg_3 [7:0];
 logic [7:0] valid_reg;
 logic [TAG_WIDTH-1:0] tag_reg [7:0];
+logic dirty_reg [7:0];
+
 
 always_ff @ (negedge clk) begin
-    if (WEC) begin
+    if (WEC && !hit) begin
         if (WEE) begin
             case {block_offset}
                 2'b00: begin
                     data_reg_0[set_num] <= DATA_IN;
                     tag_reg[set_num] <= tag;
                     valid_reg[set_num] <= 1'b1;
+                    dirty_reg[set_num] <= 1'b1;
                 end
                 2'b01: begin
                     data_reg_1[set_num] <= DATA_IN;
                     tag_reg[set_num] <= tag;
                     valid_reg[set_num] <= 1'b1;
+                    dirty_reg[set_num] <= 1'b1;
                 end
                 2'b10: begin
                     data_reg_2[set_num] <= DATA_IN;
                     tag_reg[set_num] <= tag;
                     valid_reg[set_num] <= 1'b1;
+                    dirty_reg[set_num] <= 1'b1;
                 end
                 2'b11: begin
                     data_reg_3[set_num] <= DATA_IN;
                     tag_reg[set_num] <= tag;
                     valid_reg[set_num] <= 1'b1;
+                    dirty_reg[set_num] <= 1'b1;
                 end
                 default: begin
                     data_reg_0[set_num] <= 32'b0;
@@ -56,6 +63,7 @@ always_ff @ (negedge clk) begin
                     data_reg_3[set_num] <= 32'b0;
                     tag_reg[set_num] <= 25'b0;
                     valid_reg[set_num] <= 1'b0;
+                    dirty_reg[set_num] <= 1'b1;
                 end
             endcase
         end
@@ -66,6 +74,7 @@ always_ff @ (negedge clk) begin
         data_reg_3[set_num] <= DATA_IN_3;
         tag_reg[set_num] <= tag;
         valid_reg[set_num] <= 1'b1;
+        dirty_reg[set_num] <= 1'b0;
         end
     end
 end
