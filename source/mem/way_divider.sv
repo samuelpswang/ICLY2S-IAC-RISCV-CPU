68313ad9b1653109afbfb783271a2e21338b4256
Chenglin-Sun <116260478+Chenglin-Sun@users.noreply.github.com>
2022-12-12 12:28:40 +0000

    feat: add way_divider.sv

---
+++ source/mem/way_divider.sv
@@ -0,0 +1,23 @@
+module way_divider (
+  input logic clk,
+  input logic WEC, // Write enable for cache
+  input logic [2:0] set_num,
+  output logic U // LRU replacement policy
+);
+
+logic [7:0] Ureg;
+
+always_ff @ (posedge clk) begin
+  if (WEC) begin
+      if (!Ureg[set_num])
+        Ureg[set_num] <= 1'b1;
+      else
+        Ureg[set_num] <= 1'b0;
+  end
+end
+
+always_comb begin
+  U = (Ureg[set_num]) ? 1'b1 : 1'b0;
+end
+
+endmodule
