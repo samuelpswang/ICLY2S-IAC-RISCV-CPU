83df1ba3be10b4d0b6a5d0e5bf85026076c2cbc7
Chenglin-Sun <116260478+Chenglin-Sun@users.noreply.github.com>
2022-12-13 01:41:38 +0000

    chore: update top file for new signals

--- source/mem/cached_memory_2way_top.sv
+++ source/mem/cached_memory_2way_top.sv
@@ -18,12 +18,28 @@ logic [24:0] T1;
 logic [24:0] T2;
 logic V1;
 logic V2;
+logic [DATA_WIDTH-1:0] DATA_OUT_CACHE_0;
 logic [DATA_WIDTH-1:0] DATA_OUT_CACHE_1;
-logic [DATA_WIDTH-1:0] DATA_OUT_CACHE_2;
 logic hit;
 logic U;
+logic Cache_0_Data_Mem_WE;
+logic Cache_1_Data_Mem_WE;
+logic [DATA_WIDTH-1:0] Cache_0_WD0;
+logic [DATA_WIDTH-1:0] Cache_0_WD1;
+logic [DATA_WIDTH-1:0] Cache_0_WD2;
+logic [DATA_WIDTH-1:0] Cache_0_WD3;
+logic [DATA_WIDTH-1:0] Cache_1_WD0;
+logic [DATA_WIDTH-1:0] Cache_1_WD1;
+logic [DATA_WIDTH-1:0] Cache_1_WD2;
+logic [DATA_WIDTH-1:0] Cache_1_WD3;
+logic [DATA_WIDTH-1:0] WD0;
+logic [DATA_WIDTH-1:0] WD1;
+logic [DATA_WIDTH-1:0] WD2;
+logic [DATA_WIDTH-1:0] WD3;
+logic Data_Mem_WE;
 
-data_cache data_cache_1(
+
+data_cache data_cache_0(
     .clk(clk),
     .tag(A[31:7]),
     .set_num(A[6:4]),
@@ -38,10 +54,15 @@ data_cache data_cache_1(
     .DATA_IN_3(RD_3),
     .TAG(T1),
     .V(V1),
-    .DATA_OUT(DATA_OUT_CACHE_1)
+    .DATA_OUT(DATA_OUT_CACHE_0),
+    .DATA_MEM_WE(Cache_0_Data_Mem_WE),
+    .DATA_OUT_0(Cache_0_WD0),
+    .DATA_OUT_1(Cache_0_WD1),
+    .DATA_OUT_2(Cache_0_WD2),
+    .DATA_OUT_3(Cache_0_WD3)
 );
 
-data_cache data_cache_2(
+data_cache data_cache_1(
     .clk(clk),
     .tag(A[31:7]),
     .set_num(A[6:4]),
@@ -56,7 +77,12 @@ data_cache data_cache_2(
     .DATA_IN_3(RD_3),
     .TAG(T2),
     .V(V2),
-    .DATA_OUT(DATA_OUT_CACHE_2)
+    .DATA_OUT(DATA_OUT_CACHE_1),
+    .DATA_MEM_WE(Cache_1_Data_Mem_WE),
+    .DATA_OUT_0(Cache_1_WD0),
+    .DATA_OUT_1(Cache_1_WD1),
+    .DATA_OUT_2(Cache_1_WD2),
+    .DATA_OUT_3(Cache_1_WD3)
 );
 
 way_divider way_divider(
@@ -73,18 +99,36 @@ way_merger way_merger(
     .WE1(U),
     .TAG0(T1),
     .TAG1(T2),
-    .DATA0(DATA_OUT_CACHE_1),
-    .DATA1(DATA_OUT_CACHE_2),
+    .DATA0(DATA_OUT_CACHE_0),
+    .DATA1(DATA_OUT_CACHE_1),
     .Tag(A[31:7]),
+    .Cache_0_Data_Mem_WE(Cache_0_Data_Mem_WE),
+    .Cache_1_Data_Mem_WE(Cache_1_Data_Mem_WE),
+    .Cache_0_WD0(Cache_0_WD0),
+    .Cache_0_WD1(Cache_0_WD0),
+    .Cache_0_WD2(Cache_0_WD0),
+    .Cache_0_WD3(Cache_0_WD0),
+    .Cache_1_WD0(Cache_1_WD1),
+    .Cache_1_WD1(Cache_1_WD1),
+    .Cache_1_WD2(Cache_1_WD1),
+    .Cache_1_WD3(Cache_1_WD1),
     .DATA_OUT(DATA_OUT),
-    .hit(hit)
+    .hit(hit),
+    .Data_Mem_WE(Data_Mem_WE),
+    .WD0(WD0),
+    .WD1(WD1),
+    .WD2(WD2),
+    .WD3(WD3)
 );
 
-data_memory_cached data_memory_cached1(
+data_memory_cached data_memory_cached(
     .clk(clk),
     .A(A),
     .WE(WE),
-    .WD(WD),
+    .WD0(WD0),
+    .WD1(WD1),
+    .WD2(WD2),
+    .WD3(WD3),
     .RD0(RD_0),
     .RD1(RD_1),
     .RD2(RD_2),
