d13c11e8cfcfbf786510b8c09a337626a997cd5c
Chenglin-Sun <116260478+Chenglin-Sun@users.noreply.github.com>
2022-12-13 20:43:09 +0000

    chore: variable name fix

--- source/mem/way_divider.sv
+++ source/mem/way_divider.sv
@@ -1,6 +1,6 @@
 module way_divider (
   input logic clk,
-  input logic WEC, // Write enable for cache
+  input logic WEE, // Write enable for cache
   input logic [2:0] set_num,
   output logic U // LRU replacement policy
 );
@@ -8,7 +8,7 @@ module way_divider (
 logic [7:0] Ureg;
 
 always_ff @ (posedge clk) begin
-  if (WEC) begin
+  if (WEE) begin
       if (!Ureg[set_num])
         Ureg[set_num] <= 1'b1;
       else
